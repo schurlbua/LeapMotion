61892c5893b14e8c59a9fca8c794af9581c54e2b
Georg Baumann <georg.baumann@outlook.com>
2017-09-12 16:11:50 +0200

    first test: change cube to green when hand is detected.

---
+++ source/LeapUnitySDK2/Assets/HelloWorldTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap;
+
+public class HelloWorldTest : MonoBehaviour {
+	Controller controller;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller ();
+
+		GetComponent<Renderer>().material.color = Color.yellow;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Frame frame = controller.Frame();
+		HandList handsInFrame = frame.Hands;
+		if (handsInFrame.Count > 0) {
+			GetComponent<Renderer> ().material.color = Color.green;
+		} else {
+			GetComponent<Renderer> ().material.color = Color.red;
+		}
+
+	}
+}
