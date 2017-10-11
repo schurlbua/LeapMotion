2dde823501424add417e42697c71cf6427449fc5
Georg Baumann <georg.baumann@outlook.com>
2017-10-11 15:25:53 +0200

    Revert "zooming status: now you can move the object with a pinch"
    This reverts commit f3925781bdd41f7004d459f3f06481a10b18a252.

--- source/LeapUnitySDK2/Assets/HelloWorldTest.cs
+++ source/LeapUnitySDK2/Assets/HelloWorldTest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,23 +17,21 @@ public class HelloWorldTest : MonoBehaviour
 		controller = new Controller ();
 		GetComponent<Renderer> ().material.color = Color.yellow;
 	}
-
+	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+	{
 		Frame frame = controller.Frame ();
 		HandList handsInFrame = frame.Hands;
 		Hand hand = frame.Hands.Frontmost;
+		Bone bone = Bone.Invalid;
 		bool trigger_pinch = false;
-		bool trigger_pinch2 = false;
-		bool zooming = false;
+		FingerList allFingers = frame.Fingers;
 		Pointable pointable = frame.Pointables.Frontmost;
 		Finger finger = new Finger (pointable);
 		float THUMB_TRIGGER_DISTANCE = 0.03f;
 		Hand firstHand = handsInFrame [0];
-		Hand secondHand = handsInFrame [1];
 		Vector3 thumb_tip = firstHand.Fingers [0].TipPosition.ToUnityScaled ();
-		Vector3 thumb_tip2 = secondHand.Fingers [0].TipPosition.ToUnityScaled ();
 
 		for (int i = 1; i < firstHand.Fingers.Count && !trigger_pinch; ++i) {
 			Finger finger2 = firstHand.Fingers [i];
@@ -42,41 +40,14 @@ public class HelloWorldTest : MonoBehaviour
 				Vector3 joint_position = finger2.JointPosition ((Finger.FingerJoint)(j)).ToUnityScaled ();
 				Vector3 distance = thumb_tip - joint_position;
 				if (distance.magnitude < THUMB_TRIGGER_DISTANCE)
-				trigger_pinch = true;
+					trigger_pinch = true;
 			}
 		}
-
-		for (int i = 1; i < secondHand.Fingers.Count && !trigger_pinch2; ++i) {
-			Finger finger3 = secondHand.Fingers [i];
-
-			for (int j = 0; j < 22 && !trigger_pinch2; ++j) {
-				Vector3 joint_position2 = finger3.JointPosition ((Finger.FingerJoint)(j)).ToUnityScaled ();
-				Vector3 distance2 = thumb_tip2 - joint_position2;
-				if (distance2.magnitude < THUMB_TRIGGER_DISTANCE)
-				trigger_pinch2 = true;
-			}
-		}
-
-		/*if (handsInFrame.Count == 2 && trigger_inside && trigger_pinch && trigger_pinch2) {
-		Debug.Log ("beide");
-		//transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
-		}*/
 
 		if (handsInFrame.Count > 0) {
 			GetComponent<Renderer> ().material.color = Color.green;
 			if (finger != Finger.Invalid) {
-				if (handsInFrame.Count == 2 && trigger_inside && trigger_pinch && trigger_pinch2) {
-					Vector3 distance3 = thumb_tip - thumb_tip2;
-					float zoomdistance = 0;
-					for(int i = 0; i<3; i++){
-						if (distance3[i]>0 && distance3[i]>zoomdistance){
-							zoomdistance = distance3[i];
-						}
-					}
-					//Debug.Log(zoomdistance);
-					zooming = true;
-					//transform.localScale += distance3;
-				} else if ((trigger_pinch || trigger_pinch2) && trigger_inside && !zooming) {
+				if (trigger_pinch && trigger_inside) {
 					GetComponent<Renderer> ().material.color = Color.blue;
 					transform.position = handCtrl.transform.TransformPoint (hand.Fingers [0].TipPosition.ToUnityScaled ()); //translation
 				} else {
@@ -89,9 +60,8 @@ public class HelloWorldTest : MonoBehaviour
 
 	}
 
-	void OnTriggerEnter (Collider other)
-	{
-		//Debug.Log (other.gameObject.name);
+	void OnTriggerEnter (Collider other) {
+		Debug.Log (other.gameObject.name);
 		trigger_inside = true;
 	}
 }
