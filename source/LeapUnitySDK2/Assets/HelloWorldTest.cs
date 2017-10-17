10078fe54c37c19ff368d84b212b3361032a07ca
Georg Baumann <georg.baumann@outlook.com>
2017-10-17 15:29:00 +0200

    implementation of the translation pinch function

--- source/LeapUnitySDK2/Assets/HelloWorldTest.cs
+++ source/LeapUnitySDK2/Assets/HelloWorldTest.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Leap;
 
 public class HelloWorldTest : MonoBehaviour
@@ -11,38 +8,35 @@ public class HelloWorldTest : MonoBehaviour
 	bool trigger_inside = false;
 	public HandController handCtrl;
 
-	// Use this for initialization
-	void Start ()
-	{
+	void Start ()	{
 		controller = new Controller ();
 		GetComponent<Renderer> ().material.color = Color.yellow;
 	}
 
-	// Update is called once per frame
 	void Update () {
-
 		Frame frame = controller.Frame ();
 		HandList handsInFrame = frame.Hands;
 		Hand hand = frame.Hands.Frontmost;
-		bool trigger_pinch = false;
+		bool trigger_pinch1 = false;
 		bool trigger_pinch2 = false;
 		bool zooming = false;
-		Pointable pointable = frame.Pointables.Frontmost;
-		Finger finger = new Finger (pointable);
+		Finger finger = new Finger (frame.Pointables.Frontmost);
 		float THUMB_TRIGGER_DISTANCE = 0.03f;
 		Hand firstHand = handsInFrame [0];
 		Hand secondHand = handsInFrame [1];
-		Vector3 thumb_tip = firstHand.Fingers [0].TipPosition.ToUnityScaled ();
+		Vector3 thumb_tip1 = firstHand.Fingers [0].TipPosition.ToUnityScaled ();
 		Vector3 thumb_tip2 = secondHand.Fingers [0].TipPosition.ToUnityScaled ();
 
-		for (int i = 1; i < firstHand.Fingers.Count && !trigger_pinch; ++i) {
+		// checking if hands pinch
+
+		for (int i = 1; i < firstHand.Fingers.Count && !trigger_pinch1; ++i) {
 			Finger finger2 = firstHand.Fingers [i];
 
-			for (int j = 0; j < 22 && !trigger_pinch; ++j) {
+			for (int j = 0; j < 22 && !trigger_pinch1; ++j) {
 				Vector3 joint_position = finger2.JointPosition ((Finger.FingerJoint)(j)).ToUnityScaled ();
-				Vector3 distance = thumb_tip - joint_position;
+				Vector3 distance = thumb_tip1 - joint_position;
 				if (distance.magnitude < THUMB_TRIGGER_DISTANCE)
-				trigger_pinch = true;
+				trigger_pinch1 = true;
 			}
 		}
 
@@ -57,16 +51,14 @@ public class HelloWorldTest : MonoBehaviour
 			}
 		}
 
-		/*if (handsInFrame.Count == 2 && trigger_inside && trigger_pinch && trigger_pinch2) {
-		Debug.Log ("beide");
-		//transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
-		}*/
+		// executing the movement
 
 		if (handsInFrame.Count > 0) {
 			GetComponent<Renderer> ().material.color = Color.green;
 			if (finger != Finger.Invalid) {
-				if (handsInFrame.Count == 2 && trigger_inside && trigger_pinch && trigger_pinch2) {
-					Vector3 distance3 = thumb_tip - thumb_tip2;
+				//zoomfunction - not working yet
+				if (handsInFrame.Count == 2 && trigger_inside && trigger_pinch1 && trigger_pinch2) {
+					Vector3 distance3 = thumb_tip1 - thumb_tip2;
 					float zoomdistance = 0;
 					for(int i = 0; i<3; i++){
 						if (distance3[i]>0 && distance3[i]>zoomdistance){
@@ -76,7 +68,9 @@ public class HelloWorldTest : MonoBehaviour
 					//Debug.Log(zoomdistance);
 					zooming = true;
 					//transform.localScale += distance3;
-				} else if ((trigger_pinch || trigger_pinch2) && trigger_inside && !zooming) {
+				}
+				// translation function -  if hand is inside and pinched -> you can move the object
+				else if ((trigger_pinch1 || trigger_pinch2) && trigger_inside && !zooming) {
 					GetComponent<Renderer> ().material.color = Color.blue;
 					transform.position = handCtrl.transform.TransformPoint (hand.Fingers [0].TipPosition.ToUnityScaled ()); //translation
 				} else {
@@ -89,9 +83,10 @@ public class HelloWorldTest : MonoBehaviour
 
 	}
 
+	// checking if hands are colliding with the object
+
 	void OnTriggerEnter (Collider other)
 	{
-		//Debug.Log (other.gameObject.name);
 		trigger_inside = true;
 	}
 }
