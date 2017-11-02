e6a06489cb9289788fb2c3af82bafb1aa75f1276
hubert <hubert.marret@gmail.com>
2017-11-02 15:58:17 +0100

    Debugged rotation : the model is not following the hand, it's rotating with the delta rotation of the hand

--- source/LeapUnitySDK2/Assets/Scrips/Rotation.cs
+++ source/LeapUnitySDK2/Assets/Scrips/Rotation.cs
@@ -9,35 +9,47 @@ public class Rotation : Gesture {
 	Controller controller;
 	public HandController handCtrl;
 
+	private Quaternion oldHandRotation;
 
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 		controller = new Controller ();
+		oldHandRotation = Quaternion.identity;
 	}
 
-	private bool IsHand(Collider other)
-	{
+	private bool IsHand(Collider other) {
 		if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel> ())
 			return true;
 		else
 			return false;
 	}
 
-	void OnTriggerStay(Collider other) 
-	{
+	void OnTriggerEnter() {
+		oldHandRotation = Quaternion.identity;
+	}
+
+	void OnTriggerStay(Collider other) {
 		if (IsHand (other)) 
 		{
 			Frame frame = controller.Frame (); 
 			Hand hand = frame.Hands.Frontmost;
 
-			transform.rotation = handCtrl.transform.rotation * hand.Basis.Rotation (false); //rotation
-			Activated = true;
+			Quaternion newHandRotation = handCtrl.transform.rotation * hand.Basis.Rotation (false);
+
+			if (oldHandRotation != Quaternion.identity) {
+				Quaternion deltaRotation = newHandRotation * Quaternion.Inverse (oldHandRotation);
+				transform.rotation = deltaRotation * transform.rotation;
+
+				//transform.rotation = handCtrl.transform.rotation * hand.Basis.Rotation (false); //rotation
+				Activated = true;
+			}
+
+			oldHandRotation = handCtrl.transform.rotation * hand.Basis.Rotation (false);
 		}
 	}
 
-	void OnTriggerExit(Collider other)
-	{
+	void OnTriggerExit() {
 		Activated = false;
+		oldHandRotation = Quaternion.identity;
 	}
 }
